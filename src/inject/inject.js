fcfb99dbc56a6f30c71862a209f801044669eef2
Ashley Cutmore <ashley.cutmore@gmail.com>
2016-02-26 15:01:16 +0000

    Can edit issue title and description from modal. Shows a spiner during requests

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,13 +10,14 @@ chrome.extension.sendMessage({}, (response) => {
 
 function init(){
   var modal_html = '<div id="bbbi-modal">' +
-	 '<span class="bbbi-modal-title">No issue</span><br>' +
-	'<span class="bbbi-modal-body"></span><br>' +
+	 '<span class="bbbi-modal-title bbbi-editable">No issue</span><br>' +
+	'<span class="bbbi-modal-body bbbi-editable"></span><br>' +
 	'<span class="bbbi-modal-actions">' +
 	'<div class="aui-buttons">' +
 	    '<a id="bbbi-open" class="aui-button aui-button-primary" href="">Open</a>' +
 	    '<a id="bbbi-resolve" class="aui-button aui-button-primary" href="">Resolve</a>' +
 	'</div>' +
+	'</span>' +
 	'</div>';
 
 	$('body').append(modal_html);
@@ -26,25 +27,55 @@ function init(){
 		overlay: 0.2
 	});
 
+	$('body').append('<div class="bbbi-spinner"></div>');
+	spin(false);
+
 	$('#bbbi-open', modal).click((e) => {
 		e.preventDefault();
-		updateIssue(modal.active_issue_number, {"status": "open"});
+		updateIssue(modal.issue_number, {"status": "open"}, true);
 	});
 
 	$('#bbbi-resolve', modal).click((e) => {
 		e.preventDefault();
-		updateIssue(modal.active_issue_number, {"status": "resolved"});
+		updateIssue(modal.issue_number, {"status": "resolved"}, true);
+	});
+
+	$('.bbbi-modal-title', modal).click((e) => {
+		e.preventDefault();
+		if (modal.active_issue_number == null)
+		  return;
+
+		var update = window.prompt("New Title", modal.issue.title);
+
+		if (update != null){
+			updateIssue(modal.active_issue_number, {"title": update}, true);
+		}
+	});
+
+	$('.bbbi-modal-body', modal).click((e) => {
+		e.preventDefault();
+		if (modal.active_issue_number == null)
+		  return;
+
+		var update = window.prompt("New Description", modal.issue.content);
+
+		if (update != null){
+			updateIssue(modal.active_issue_number, {"content": update}, true);
+		}
 	});
 
 	modal.update = function(issueNumber, force){
+		modal.issue_number = issueNumber;
     if (force || modal.active_issue_number != issueNumber){
 			modal.active_issue_number = null;
+			modal.issue = null;
 			$('.bbbi-modal-title', modal).html("Issue #" + issueNumber);
 			$('.bbbi-modal-body', modal).html("...");
 
 			var url = getURL("/" + issueNumber)
 			request(url).then((issue) => {
 				modal.active_issue_number = issueNumber;
+				modal.issue = issue;
 				$('.bbbi-modal-title', modal).html("Issue #" + issueNumber + " " +	issue.title);
 				$('.bbbi-modal-body', modal).html(issue.content);
 			});
@@ -94,7 +125,9 @@ function request(url, init, headers, no_retry){
 
 	init.headers = headers;
 
+  spin(true);
 	return fetch(url, init).then((response) => {
+		spin(false);
 		if (response.ok)
 	    return response.json();
 		else if (response.status == 401 && !no_retry){
@@ -105,6 +138,9 @@ function request(url, init, headers, no_retry){
 		}
 		else
 		  throw new Error("unknown request response state");
+	}).catch((e) => {
+		spin(false);
+		return Promise.reject(e);
 	});
 }
 
@@ -117,7 +153,7 @@ function login(){
 	});
 }
 
-function updateIssue(issueNumber, update){
+function updateIssue(issueNumber, update, reload){
 	if (issueNumber != null){
 
 		var headers = new Headers();
@@ -125,13 +161,27 @@ function updateIssue(issueNumber, update){
 
 		var url = getURL("/" + issueNumber);
 
-		request(url, {
+		return request(url, {
 			  method: 'PUT',
 			  body: JSON.stringify(update)
 		  },
 	    headers
 		).then((issue) => {
+			if (reload)
 		     location.reload();
+			else {
+				return issue;
+			}
 		});
 	}
+
+	return Promise.reject('no issue number');
+}
+
+function spin(show){
+	var s = $('.bbbi-spinner');
+	if (show)
+	   s.show();
+	 else
+	   s.hide()
 }
