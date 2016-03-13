4d38f3f4c6d155e579477007c29ac2f96339ddff
Ashley Cutmore <ashley.cutmore@gmail.com>
2016-03-13 16:10:37 +0000

    Store AMD loader compatible

--- extension/src/options_custom/lib/store.js
+++ extension/src/options_custom/lib/store.js
@@ -4,10 +4,10 @@
 // License: MIT-license
 //
 (function () {
-    var Store = this.Store = function (name, defaults) {
+    var Store = function (name, defaults) {
         var key;
         this.name = name;
-        
+
         if (defaults !== undefined) {
             for (key in defaults) {
                 if (defaults.hasOwnProperty(key) && this.get(key) === undefined) {
@@ -16,7 +16,16 @@
             }
         }
     };
-    
+
+    if(typeof define === "function" && define.amd) {
+      define([], function () {
+        return Store;
+      });
+    }
+    else {
+      this.Store = Store; // Make global
+    }
+
     Store.prototype.get = function (name) {
         name = "store." + this.name + "." + name;
         if (localStorage.getItem(name) === null) { return undefined; }
@@ -26,7 +35,7 @@
             return null;
         }
     };
-    
+
     Store.prototype.set = function (name, value) {
         if (value === undefined) {
             this.remove(name);
@@ -40,39 +49,39 @@
                     value = null;
                 }
             }
-            
+
             localStorage.setItem("store." + this.name + "." + name, value);
         }
-        
+
         return this;
     };
-    
+
     Store.prototype.remove = function (name) {
         localStorage.removeItem("store." + this.name + "." + name);
         return this;
     };
-    
+
     Store.prototype.removeAll = function () {
         var name,
             i;
-        
+
         name = "store." + this.name + ".";
         for (i = (localStorage.length - 1); i >= 0; i--) {
             if (localStorage.key(i).substring(0, name.length) === name) {
                 localStorage.removeItem(localStorage.key(i));
             }
         }
-        
+
         return this;
     };
-    
+
     Store.prototype.toObject = function () {
         var values,
             name,
             i,
             key,
             value;
-        
+
         values = {};
         name = "store." + this.name + ".";
         for (i = (localStorage.length - 1); i >= 0; i--) {
@@ -82,10 +91,10 @@
                 if (value !== undefined) { values[key] = value; }
             }
         }
-        
+
         return values;
     };
-    
+
     Store.prototype.fromObject = function (values, merge) {
         if (merge !== true) { this.removeAll(); }
         for (var key in values) {
@@ -93,7 +102,7 @@
                 this.set(key, values[key]);
             }
         }
-        
+
         return this;
     };
 }());
