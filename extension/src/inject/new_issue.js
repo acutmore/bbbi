5f226286cb89fff194f59e2b5bb4c1caaf38ccc3
Ashley Cutmore <ashley.cutmore@gmail.com>
2016-06-14 21:59:45 +0100

    NEW - Milestones on new issue page that start with /[\d]_/ are moved to the end

--- extension/src/inject/new_issue.js
+++ extension/src/inject/new_issue.js
@@ -4,29 +4,53 @@
     // Move ones in the past to the end of the list
     var $milestoneSelect = $('#id_milestone');
     
-    $milestoneSelect.children('option').each((i, e) => {
+    function moveToEnd(predicate) {
+        $milestoneSelect.children('option').each((i, e) => {
+            if (predicate(e)){
+                e.remove();
+                $milestoneSelect[0].appendChild(e);
+            }
+        });
+    }
+    
+    // Passed dates
+    moveToEnd((e) => {
 
         try {
             var dateStr = e.innerText.match(/\d\d\d\d[_-]\d\d[_-]\d\d/)[0];
         }
-        catch (e) {
+        catch (err) {
             // Ignore
-            return;
+            return false;
         }
 
         if (dateStr != null) {
-
             [yyyy, mm, dd] = dateStr.split(/[_-]/).map(s => parseInt(s));
-            var mileStoneDate = new Date(yyyy, mm - 1, dd, 23,59);
-            
-            if (mileStoneDate.getTime() < Date.now()){
-                console.log("BBBI - moving milestone to end of list", e.innerText);
-                e.remove();
-                $milestoneSelect[0].appendChild(e);
-            }
+            var mileStoneDate = new Date(yyyy, mm - 1, dd, 23, 59);
 
+            if (mileStoneDate.getTime() < Date.now()) {
+                return true;
+            }
         }
 
+        return false;
     });
+    
+    // Numeric  (starts with '3_' etc)
+    moveToEnd((e) => {
+        try {
+            if (e.innerText.match(/^[\d]_/) != null){
+                if (parseInt(e.innerText.match(/^[\d]/)[0]) >= 3){
+                    return true;
+                }
+            };
+        }
+        catch (err){
+            // Ignore
+        }
+        return false;
+    });
+    
+    
 })();
 
